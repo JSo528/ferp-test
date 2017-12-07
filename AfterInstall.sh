2870a745221905ffa60198a6f9948e7d7012edfe
jso528 <jso528@gmail.com>
2017-12-06 19:18:17 -0800

    cleaned up shell scripts

--- AfterInstall.sh
+++ AfterInstall.sh
@@ -1,4 +1,3 @@
 source /home/ec2-user/.bash_profile
-# ln -s /usr/local/bin/npm /usr/bin/npm
 cd /home/ec2-user/app
 npm install
--- ApplicationStart.sh
+++ ApplicationStart.sh
@@ -1,3 +1,3 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app
-pm2 start app.js
+pm2 restart app
