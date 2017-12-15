b23531ba4517c387b6a9ebceab3182142402110b
jso528 <jso528@gmail.com>
2017-12-15 09:18:59 -0800

    updated for baseball

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var PORT = 3000;
+var PORT = 3001;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
